7e583cc08944b40a791fad5f89d1c070036590a6
leeosilvp <leo965679@gmail.com>
2025-09-20 18:43:37 -0300

    5 a- criando e estilizando Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
+import '../css/header.css'
+import icon from '../assets/img/icon-dark.svg'
 
 function Header() {
   return (
-    <div>
-      
-    </div>
+    <header>
+      <h1>CVHub</h1>
+      <img src={icon}/>
+      <a href="#"><i className="fa-brands fa-square-github"></i></a>
+    </header>
   )
 }
 
