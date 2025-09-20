d2a86b9fb314ce570a4673c851187798430418ee
leeosilvp <leo965679@gmail.com>
2025-09-20 18:33:06 -0300

    4 a- criando opções de temas

--- src/components/Themes.jsx
+++ src/components/Themes.jsx
@@ -1,9 +1,28 @@
+import '../css/themes.css'
+import icondark from '../assets/img/icon-dark.svg'
+import iconlight from '../assets/img/icon-light.svg'
+
 
 function Themes() {
   return (
-    <div>
-      
-    </div>
+    <section className="ctn-themes">
+      <h1>select a theme:</h1>
+      <article className="theme">
+        <input type="checkbox" />
+        <div className="card-theme">
+          <img src={icondark}/>
+          <h1>GitHub - Dark</h1>
+        </div>
+      </article>
+
+      <article className="theme">
+        <input type="checkbox" />
+        <div className="card-theme">
+          <img src={iconlight}/>
+          <h1>GitHub - Light</h1>
+        </div>
+      </article>
+    </section>
   )
 }
 
